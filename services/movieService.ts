55b39512142c336780e7611cf636c55a1273f88d
hamed-bavar <54584195+hamed-bavar@users.noreply.github.com>
2021-11-14 15:50:24 +0330

    Update movieService.ts

--- services/movieService.ts
+++ services/movieService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { MoviesType, MovieDetails, Credits, Gallery, SearchResults } from "../types/types";
 class MovieService {
   url: string = `https://api.themoviedb.org/3/movie/category?api_key=c46e93100eb64667d419552d00b518d6&language=en-US&page=pageNumber`;
-  newUrl: string = `https://movie-galaxy-proxy.herokuapp.com/movies/category/pageNumber`;
+  newUrl: string = `https://rev-proxy-movie.herokuapp.com/movies/category/pageNumber`;
   async getMovieByCatAndPage(category: string, pageNumber: number) {
     const actualReq = this.newUrl
       .replace("category", category)
